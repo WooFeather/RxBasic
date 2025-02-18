cbd93c27aef2e18d1fc4e71431eb69035c54d540
Woohyun Jo <bluewing1004@gmail.com>
2025-02-18 20:04:37 +0900

    [Feat] ValidationVC Rx로 구현

--- RxBasic/Validation/ValidationViewController.swift
+++ RxBasic/Validation/ValidationViewController.swift
@@ -31,7 +31,47 @@ final class ValidationViewController: UIViewController {
     }
 
     private func bind() {
+        let usernameValid = usernameTextField.rx.text.orEmpty
+            .withUnretained(self)
+            .map { owner, value in
+                value.count >= owner.minimalUsernameLength
+            }
+            .share(replay: 1) // share(replay:)의 의미는?
         
+        let passwordValid = passwordTextField.rx.text.orEmpty
+            .withUnretained(self)
+            .map { owner, value in
+            value.count >= owner.minimalPasswordLength
+            }
+            .share(replay: 1)
+        
+        let everythingValid = Observable.combineLatest(usernameValid, passwordValid) { $0 && $1 }
+            .share(replay: 1)
+        
+        // username의 유효성이 password TF의 isEnabled의 상태와 같도록 함
+        usernameValid
+            .bind(to: passwordTextField.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        usernameValid
+            .bind(to: usernameValidationLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        passwordValid
+            .bind(to: passwordValidationLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        everythingValid
+            .bind(to: signInButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        signInButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.showAlert(message: "회원가입 성공!") {
+                    owner.dismiss(animated: true)
+                }
+            }
+            .disposed(by: disposeBag)
     }
 }
 
@@ -81,7 +121,7 @@ extension ValidationViewController {
         signInButton.snp.makeConstraints { make in
             make.top.equalTo(passwordValidationLabel.snp.bottom).offset(30)
             make.horizontalEdges.equalToSuperview().inset(12)
-            make.height.equalTo(50)
+            make.height.equalTo(44)
         }
         
         usernameLabel.text = "Username"
@@ -89,16 +129,19 @@ extension ValidationViewController {
         usernameTextField.borderStyle = .roundedRect
         usernameValidationLabel.text = "Username은 최소 \(minimalUsernameLength)글자 이상이어야 합니다"
         usernameValidationLabel.font = .systemFont(ofSize: 17)
+        usernameValidationLabel.textColor = .red
         
         passwordLabel.text = "Password"
         passwordLabel.font = .systemFont(ofSize: 17)
         passwordTextField.borderStyle = .roundedRect
         passwordValidationLabel.text = "Username은 최소 \(minimalPasswordLength)글자 이상이어야 합니다"
         passwordValidationLabel.font = .systemFont(ofSize: 17)
+        passwordValidationLabel.textColor = .red
         
         signInButton.backgroundColor = .systemTeal
         signInButton.setTitle("회원가입", for: .normal)
         signInButton.setTitleColor(.white, for: .normal)
+        signInButton.setTitleColor(.lightGray, for: .disabled)
         signInButton.layer.cornerRadius = 10
     }
 }
